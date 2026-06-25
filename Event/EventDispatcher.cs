dc23fdecba7eb2563b65bda1b0197e0515bd79eb
AI-Creator <cd_huanghz@seasky.com>
2026-06-25 21:32:39 +0800

    Update EventDispatcher

--- Event/EventDispatcher.cs
+++ Event/EventDispatcher.cs
@@ -1,56 +1,229 @@
 using System;
+using System.Collections.Generic;
+using FinchUtils.Debugger;
 
 namespace FinchUtils.Event
 {
     public static class EventDispatcher {
-        private static readonly EventInternal EventInternal = new();
+        private static readonly EventRouter Router = new();
 
         public static void RegEventListener(string eventType, Action handler) {
-            EventInternal.RegEventListener(eventType, handler);
+            Router.RegEventListener(eventType, handler);
         }
 
         public static void RegEventListener<T>(string eventType, Action<T> handler) {
-            EventInternal.RegEventListener(eventType, handler);
+            Router.RegEventListener(eventType, handler);
         }
 
         public static void RegEventListener<T1, T2>(string eventType, Action<T1, T2> handler) {
-            EventInternal.RegEventListener(eventType, handler);
+            Router.RegEventListener(eventType, handler);
         }
 
         public static void RegEventListener<T1, T2, T3>(string eventType, Action<T1, T2, T3> handler) {
-            EventInternal.RegEventListener(eventType, handler);
+            Router.RegEventListener(eventType, handler);
         }
 
         public static void UnRegEventListener(string eventType, Action handler) {
-            EventInternal.UnRegEventListener(eventType, handler);
+            Router.UnRegEventListener(eventType, handler);
         }
 
         public static void UnRegEventListener<T>(string eventType, Action<T> handler) {
-            EventInternal.UnRegEventListener(eventType, handler);
+            Router.UnRegEventListener(eventType, handler);
         }
 
         public static void UnRegEventListener<T1, T2>(string eventType, Action<T1, T2> handler) {
-            EventInternal.UnRegEventListener(eventType, handler);
+            Router.UnRegEventListener(eventType, handler);
         }
 
         public static void UnRegEventListener<T1, T2, T3>(string eventType, Action<T1, T2, T3> handler) {
-            EventInternal.UnRegEventListener(eventType, handler);
+            Router.UnRegEventListener(eventType, handler);
         }
 
         public static void TriggerEvent(string eventType) {
-            EventInternal.TriggerEvent(eventType);
+            Router.TriggerEvent(eventType);
         }
 
         public static void TriggerEvent<T>(string eventType, T arg1) {
-            EventInternal.TriggerEvent(eventType, arg1);
+            Router.TriggerEvent(eventType, arg1);
         }
 
         public static void TriggerEvent<T1, T2>(string eventType, T1 arg1, T2 arg2) {
-            EventInternal.TriggerEvent(eventType, arg1, arg2);
+            Router.TriggerEvent(eventType, arg1, arg2);
         }
 
         public static void TriggerEvent<T1, T2, T3>(string eventType, T1 arg1, T2 arg2, T3 arg3) {
-            EventInternal.TriggerEvent(eventType, arg1, arg2, arg3);
+            Router.TriggerEvent(eventType, arg1, arg2, arg3);
+        }
+
+        private sealed class EventRouter {
+            private readonly Dictionary<string, Delegate> _eventRouter = new();
+
+            private void OnListenerAdding(string eventType, Delegate listenerAdded) {
+                _eventRouter.TryAdd(eventType, null);
+
+                Delegate eventDelegate = _eventRouter[eventType];
+                if (eventDelegate != null && eventDelegate.GetType() != listenerAdded.GetType()) {
+                    throw new Exception(
+                        $"Try to add not correct event {eventType}. Current type is {eventDelegate.GetType().Name}, adding type is {listenerAdded.GetType().Name}");
+                }
+            }
+
+            private bool OnListenerRemoving(string eventType, Delegate listenerRemoved) {
+                if (!_eventRouter.TryGetValue(eventType, out Delegate eventDelegate)) {
+                    return false;
+                }
+
+                if (eventDelegate != null && eventDelegate.GetType() != listenerRemoved.GetType()) {
+                    throw new Exception(
+                        $"Remove listener {eventType} failed, Current type is {eventDelegate.GetType()}, adding type is {listenerRemoved.GetType()}.");
+                }
+
+                return true;
+            }
+
+            private void OnListenerRemoved(string eventType) {
+                if (_eventRouter.ContainsKey(eventType) && _eventRouter[eventType] == null) {
+                    _eventRouter.Remove(eventType);
+                }
+            }
+
+            public void RegEventListener(string eventType, Action handler) {
+                OnListenerAdding(eventType, handler);
+                _eventRouter[eventType] = (Action)Delegate.Combine((Action)_eventRouter[eventType], handler);
+            }
+
+            public void RegEventListener<T>(string eventType, Action<T> handler) {
+                OnListenerAdding(eventType, handler);
+                _eventRouter[eventType] = (Action<T>)Delegate.Combine((Action<T>)_eventRouter[eventType], handler);
+            }
+
+            public void RegEventListener<T1, T2>(string eventType, Action<T1, T2> handler) {
+                OnListenerAdding(eventType, handler);
+                _eventRouter[eventType] = (Action<T1, T2>)Delegate.Combine((Action<T1, T2>)_eventRouter[eventType], handler);
+            }
+
+            public void RegEventListener<T1, T2, T3>(string eventType, Action<T1, T2, T3> handler) {
+                OnListenerAdding(eventType, handler);
+                _eventRouter[eventType] =
+                    (Action<T1, T2, T3>)Delegate.Combine((Action<T1, T2, T3>)_eventRouter[eventType], handler);
+            }
+
+            public void UnRegEventListener(string eventType, Action handler) {
+                if (!OnListenerRemoving(eventType, handler)) {
+                    return;
+                }
+
+                _eventRouter[eventType] = (Action)Delegate.Remove((Action)_eventRouter[eventType], handler);
+                OnListenerRemoved(eventType);
+            }
+
+            public void UnRegEventListener<T>(string eventType, Action<T> handler) {
+                if (!OnListenerRemoving(eventType, handler)) {
+                    return;
+                }
+
+                _eventRouter[eventType] = (Action<T>)Delegate.Remove((Action<T>)_eventRouter[eventType], handler);
+                OnListenerRemoved(eventType);
+            }
+
+            public void UnRegEventListener<T1, T2>(string eventType, Action<T1, T2> handler) {
+                if (!OnListenerRemoving(eventType, handler)) {
+                    return;
+                }
+
+                _eventRouter[eventType] = (Action<T1, T2>)Delegate.Remove((Action<T1, T2>)_eventRouter[eventType], handler);
+                OnListenerRemoved(eventType);
+            }
+
+            public void UnRegEventListener<T1, T2, T3>(string eventType, Action<T1, T2, T3> handler) {
+                if (!OnListenerRemoving(eventType, handler)) {
+                    return;
+                }
+
+                _eventRouter[eventType] =
+                    (Action<T1, T2, T3>)Delegate.Remove((Action<T1, T2, T3>)_eventRouter[eventType], handler);
+                OnListenerRemoved(eventType);
+            }
+
+            public void TriggerEvent(string eventType) {
+                if (!_eventRouter.TryGetValue(eventType, out var @delegate)) {
+                    return;
+                }
+
+                Delegate[] invocationList = @delegate.GetInvocationList();
+                foreach (var invocation in invocationList) {
+                    if (invocation is not Action action) {
+                        throw new Exception($"TriggerEvent {eventType} error: types of parameters are not match.");
+                    }
+
+                    try {
+                        action();
+                    }
+                    catch (Exception ex) {
+                        Logger.Instance?.Error(ex.ToString());
+                    }
+                }
+            }
+
+            public void TriggerEvent<T>(string eventType, T arg1) {
+                if (!_eventRouter.TryGetValue(eventType, out var @delegate)) {
+                    return;
+                }
+
+                Delegate[] invocationList = @delegate.GetInvocationList();
+                foreach (var invocation in invocationList) {
+                    if (invocation is not Action<T> action) {
+                        throw new Exception($"TriggerEvent {eventType} error: types of parameters are not match.");
+                    }
+
+                    try {
+                        action(arg1);
+                    }
+                    catch (Exception ex) {
+                        Logger.Instance?.Error(ex.ToString());
+                    }
+                }
+            }
+
+            public void TriggerEvent<T1, T2>(string eventType, T1 arg1, T2 arg2) {
+                if (!_eventRouter.TryGetValue(eventType, out var @delegate)) {
+                    return;
+                }
+
+                Delegate[] invocationList = @delegate.GetInvocationList();
+                foreach (var invocation in invocationList) {
+                    if (invocation is not Action<T1, T2> action) {
+                        throw new Exception($"TriggerEvent {eventType} error: types of parameters are not match.");
+                    }
+
+                    try {
+                        action(arg1, arg2);
+                    }
+                    catch (Exception ex) {
+                        Logger.Instance?.Error(ex.ToString());
+                    }
+                }
+            }
+
+            public void TriggerEvent<T1, T2, T3>(string eventType, T1 arg1, T2 arg2, T3 arg3) {
+                if (!_eventRouter.TryGetValue(eventType, out var @delegate)) {
+                    return;
+                }
+
+                Delegate[] invocationList = @delegate.GetInvocationList();
+                foreach (var invocation in invocationList) {
+                    if (invocation is not Action<T1, T2, T3> action) {
+                        throw new Exception($"TriggerEvent {eventType} error: types of parameters are not match.");
+                    }
+
+                    try {
+                        action(arg1, arg2, arg3);
+                    }
+                    catch (Exception ex) {
+                        Logger.Instance?.Error(ex.ToString());
+                    }
+                }
+            }
         }
     }
 }
